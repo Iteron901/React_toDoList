384720158e57982f8d80c1151bf8c9175432782d
Itron <bochra.piotr@gmail.com>
2017-04-24 10:55:24 +0200

    generated Header and Form components

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,19 +5,35 @@ import React from 'react';
 
 let favImage = require('../images/favicon.png');
 
-class AppComponent extends React.Component {
-  render() {
-    return (
-      <div className="index">
-        <h1>To Do List</h1>
-        {/*<img src={favImage} alt="Icon" />*/}
-        {/*<div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>*/}
-      </div>
-    );
-  }
-}
+var TodoApp = React.createClass({
+	render: function(){
+		return (
+			<div>
+				<TodoHeader/>
+        <TodoForm/>
+			</div>
+		);
+	}
+});
 
-AppComponent.defaultProps = {
-};
+var TodoHeader = React.createClass({
+	render: function(){
+		return (
+			<h1>To Do List</h1>
+		);
+	}
+});
 
-export default AppComponent;
+var TodoForm = React.createClass({
+	render: function(){
+		return (
+		  <form className="col-xs-offset-1 col-xs-10 col-md-offset-3 col-md-6">
+				<input type='text' className="col-xs-10 col-sm-10" />
+				<button type='submit' className="col-xs-2 col-sm-2"><span class="glyphicon glyphicon-plus"></span></button>
+			</form>
+		);
+	}
+});
+
+TodoApp.defaultProps = {};
+export default TodoApp;

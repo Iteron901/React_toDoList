abfeec5ae61b0f3b121dadd6dc17fcb743b894a9
Itron <bochra.piotr@gmail.com>
2017-04-24 11:35:18 +0200

    li elements added (without text), options icons

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,11 +6,19 @@ import React from 'react';
 let favImage = require('../images/favicon.png');
 
 var TodoApp = React.createClass({
+  getInitialState: function(){
+		return {items: []};
+	},
+	updateItems: function(newItem){
+		var allItems = this.state.items.concat([newItem]);
+		this.setState({items: allItems});
+	},
 	render: function(){
 		return (
 			<div>
 				<TodoHeader/>
-        <TodoForm/>
+        <TodoForm onFormSubmit={this.updateItems}/>
+        <TodoList items={this.state.items}/>
 			</div>
 		);
 	}
@@ -25,12 +33,53 @@ var TodoHeader = React.createClass({
 });
 
 var TodoForm = React.createClass({
+  getInitialState: function() {
+		return {item: ''};
+	},
+	handleSubmit: function(e){
+		e.preventDefault();
+		this.props.onFormSubmit(this.state.item);
+		this.setState({item: ''});
+		React.findDOMNode(this.refs.item).focus();
+		return;
+	},
+	onChange: function(e){
+		this.setState({
+			item: e.target.value
+		});
+	},
 	render: function(){
 		return (
-		  <form className="col-xs-offset-1 col-xs-10 col-md-offset-3 col-md-6">
+		  <form className="col-xs-offset-1 col-xs-10 col-md-offset-3 col-md-6" onSubmit={this.handleSubmit}>
 				<input type='text' className="col-xs-10 col-sm-10" />
-				<button type='submit' className="col-xs-2 col-sm-2"><span class="glyphicon glyphicon-plus"></span></button>
+				<button type='submit' className="col-xs-2 col-sm-2"><span className="glyphicon glyphicon-plus"></span></button>
 			</form>
+		);
+	}
+});
+
+var TodoList = React.createClass({
+	render: function() {
+		var createItem = function(itemText) {
+			return (
+				<TodoListItem>{itemText}</TodoListItem>
+			);
+		};
+		return <ul className="col-xs-offset-1 col-xs-10 col-md-offset-3 col-md-6">{this.props.items.map(createItem)}</ul>;
+	}
+});
+
+var TodoListItem = React.createClass({
+	render: function(){
+		return (
+			<li>
+        {this.props.children}
+        <div className = "options">
+          <span className="glyphicon glyphicon-ok"></span>
+          <span className="glyphicon glyphicon-pencil"></span>
+          <span className="glyphicon glyphicon-remove"></span>
+        </div>
+      </li>
 		);
 	}
 });
